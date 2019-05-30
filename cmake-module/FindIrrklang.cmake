d06e3af6614db5181853afddf98b2756649ca490
Tomasz Gałaj <tomaszgalaj@gmail.com>
2019-05-30 08:26:02 +0200

    Added support for Win64 irrKlang

--- cmake-module/FindIrrklang.cmake
+++ cmake-module/FindIrrklang.cmake
@@ -1,22 +1,37 @@
-FIND_PATH( IRRKLANG_INCLUDE_DIR NAMES irrKlang.h PATHS
-		   "${PROJECT_SOURCE_DIR}/thirdparty/irrKlang-1.6.0/include")
-
-if(WIN32)
-	if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
-		MESSAGE(STATUS "Using MSVC!")
-		FIND_LIBRARY( IRRKLANG_LIBRARY NAMES irrKlang PATHS
-			"${PROJECT_SOURCE_DIR}/thirdparty/irrKlang-1.6.0/lib/Win32-visualStudio")
-	elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
-		MESSAGE(STATUS "Using GCC!")
-		FIND_LIBRARY( IRRKLANG_LIBRARY NAMES libirrKlang.a PATHS
-				"${PROJECT_SOURCE_DIR}/thirdparty/irrKlang-1.6.0/lib/Win32-gcc")
+if(CMAKE_CL_64)
+	FIND_PATH( IRRKLANG_INCLUDE_DIR NAMES irrKlang.h PATHS
+		   "${PROJECT_SOURCE_DIR}/thirdparty/irrKlang-64bit-1.6.0/include")
+		   
+    # 64 bits
+	if(WIN32)
+		if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
+			MESSAGE(STATUS "Using MSVC!")
+			FIND_LIBRARY( IRRKLANG_LIBRARY NAMES irrKlang PATHS
+				"${PROJECT_SOURCE_DIR}/thirdparty/irrKlang-64bit-1.6.0/lib/Winx64-visualStudio")
+		endif()
 	endif()
-elseif(APPLE)
-	FIND_LIBRARY( IRRKLANG_LIBRARY NAMES libirrklang.dylib PATHS
-		"${PROJECT_SOURCE_DIR}/thirdparty/irrKlang-1.6.0/bin/macosx-gcc")
-elseif(UNIX AND NOT APPLE)	
-	FIND_LIBRARY( IRRKLANG_LIBRARY NAMES IrrKlang PATHS
-			"${PROJECT_SOURCE_DIR}/thirdparty/irrKlang-1.6.0/bin/linux-gcc") 
+else()
+    # 32 bits
+	FIND_PATH( IRRKLANG_INCLUDE_DIR NAMES irrKlang.h PATHS
+		   "${PROJECT_SOURCE_DIR}/thirdparty/irrKlang-1.6.0/include")
+		   
+	if(WIN32)
+		if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
+			MESSAGE(STATUS "Using MSVC!")
+			FIND_LIBRARY( IRRKLANG_LIBRARY NAMES irrKlang PATHS
+				"${PROJECT_SOURCE_DIR}/thirdparty/irrKlang-1.6.0/lib/Win32-visualStudio")
+		elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+			MESSAGE(STATUS "Using GCC!")
+			FIND_LIBRARY( IRRKLANG_LIBRARY NAMES libirrKlang.a PATHS
+					"${PROJECT_SOURCE_DIR}/thirdparty/irrKlang-1.6.0/lib/Win32-gcc")
+		endif()
+	elseif(APPLE)
+		FIND_LIBRARY( IRRKLANG_LIBRARY NAMES libirrklang.dylib PATHS
+			"${PROJECT_SOURCE_DIR}/thirdparty/irrKlang-1.6.0/bin/macosx-gcc")
+	elseif(UNIX AND NOT APPLE)	
+		FIND_LIBRARY( IRRKLANG_LIBRARY NAMES IrrKlang PATHS
+				"${PROJECT_SOURCE_DIR}/thirdparty/irrKlang-1.6.0/bin/linux-gcc") 
+	endif()
 endif()
 
 SET(IRRKLANG_FOUND "NO")
